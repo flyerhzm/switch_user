d4c2b10b9d9541968fd86a63e94495cca6f87a44
Luke Cowell <lcowell@gmail.com>
2012-09-22 14:05:40 -0700

    handle all user session operations through the corresponding provider

--- app/controllers/switch_user_controller.rb
+++ app/controllers/switch_user_controller.rb
@@ -7,108 +7,51 @@ class SwitchUserController < ApplicationController
   before_filter :developer_modes_only
 
   def set_current_user
-    send("#{SwitchUser.provider}_handle", params)
+    handle_request(params)
+
     redirect_to(SwitchUser.redirect_path.call(request, params))
   end
 
   private
-    def developer_modes_only
-      render :text => "Permission Denied", :status => 403 unless available?
-    end
 
-    def available?
-      current_user = send("#{SwitchUser.provider}_current_user")
-      SwitchUser.controller_guard.call(current_user, request)
-    end
+  def developer_modes_only
+    render :text => "Permission Denied", :status => 403 unless available?
+  end
 
-    def devise_handle(params)
-      if params[:scope_identifier].blank?
-        SwitchUser.available_users.keys.each do |s|
-          warden.logout(s)
-        end
-      else
-        params[:scope_identifier] =~ /^([^_]+)_(.*)$/
-        scope, identifier = $1, $2
+  def available?
+    SwitchUser.controller_guard.call(provider.current_user, request)
+  end
 
-        SwitchUser.available_users.keys.each do |s|
-          if scope == s.to_s
-            user = find_user(scope, s, identifier)
-            warden.set_user(user, :scope => scope)
-          else
-            warden.logout(s)
-          end
-        end
+  def handle_request(params)
+    if params[:scope_identifier].blank?
+      SwitchUser.available_users.keys.each do |s|
+        provider.logout(s)
       end
-    end
+    else
+      params[:scope_identifier] =~ /^([^_]+)_(.*)$/
+      scope, identifier = $1, $2
 
-    def authlogic_handle(params)
-      if params[:scope_identifier].blank?
-        current_user_session.destroy
-      else
-        params[:scope_identifier] =~ /^([^_]+)_(.*)$/
-        scope, identifier = $1, $2
-
-        SwitchUser.available_users.keys.each do |s|
-          if scope == s.to_s
-            user = find_user(scope, s, identifier)
-            UserSession.create(user)
-          end
+      SwitchUser.available_users.keys.each do |s|
+        if scope == s.to_s
+          user = find_user(scope, s, identifier)
+          provider.login(user, scope)
+        else
+          provider.logout(s)
         end
       end
     end
+  end
 
-    def restful_authentication_handle(params)
-      if params[:scope_identifier].blank?
-        logout_killing_session!
-      else
-        params[:scope_identifier] =~ /^([^_]+)_(.*)$/
-        scope, identifier = $1, $2
-
-        SwitchUser.available_users.keys.each do |s|
-          if scope == s.to_s && user = find_user(scope, s, identifier)
-            self.current_user = user
-          end
-        end
-      end
+  def find_user(scope, identifier_scope, identifier)
+    identifier_column = SwitchUser.available_users_identifiers[identifier_scope] || :id
+    if identifier_column == :id
+      scope.classify.constantize.find(identifier)
+    else
+      scope.classify.constantize.send("find_by_#{identifier_column}!", identifier)
     end
+  end
 
-    def sorcery_handle(params)
-      if params[:scope_identifier].blank?
-        self.logout
-      else
-        params[:scope_identifier] =~ /^([^_]+)_(.*)$/
-        scope, identifier = $1, $2
-
-        SwitchUser.available_users.keys.each do |s|
-          if scope == s.to_s && user = find_user(scope, s, identifier)
-            self.auto_login(user)
-          end
-        end
-      end
-    end
-
-    def find_user(scope, identifier_scope, identifier)
-      identifier_column = SwitchUser.available_users_identifiers[identifier_scope] || :id
-      if identifier_column == :id
-        scope.classify.constantize.find(identifier)
-      else
-        scope.classify.constantize.send("find_by_#{identifier_column}!", identifier)
-      end
-    end
-
-    def devise_current_user
-      current_user
-    end
-
-    def authlogic_current_user
-      UserSession.find.try(:record)
-    end
-
-    def restful_authentication_current_user
-      current_user
-    end
-
-    def sorcery_current_user
-      current_user
-    end
+  def provider
+    Provider::Sorcery.new(self)
+  end
 end
