26c786e81f7ca6292143a3f53215be5c8ea55c45
Luke Cowell <lcowell@gmail.com>
2012-09-30 17:06:57 -0700

    remove unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,1 @@
 require 'support/provider'
-
-class ApplicationController
-
-end
-
-class Rails
-  # set version to 2 so we don't have to deal with the call to unloadable
-  def self.version
-    2
-  end
-end
