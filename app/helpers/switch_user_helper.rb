79d507511d2a86752213da83615614bd8b75cb52
Luke Cowell <lcowell@gmail.com>
2012-10-13 09:03:24 -0700

    skip method using method guard if switch_user is not currently available

--- app/helpers/switch_user_helper.rb
+++ app/helpers/switch_user_helper.rb
@@ -1,34 +1,33 @@
 module SwitchUserHelper
   def switch_user_select
-    if available?
-      options = ""
+    return unless available?
+    options = ""
 
-      options += content_tag(:option, "Guest", :value => "", :selected => !current_user)
-      SwitchUser.available_users.each do |scope, user_proc|
-        current_user = provider.current_user(scope)
-        identifier = SwitchUser.available_users_identifiers[scope]
-        name = SwitchUser.available_users_names[scope]
+    options += content_tag(:option, "Guest", :value => "", :selected => !current_user)
+    SwitchUser.available_users.each do |scope, user_proc|
+      current_user = provider.current_user(scope)
+      identifier = SwitchUser.available_users_identifiers[scope]
+      name = SwitchUser.available_users_names[scope]
 
-        user_proc.call.each do |user|
-          if current_user == user
-            options += content_tag(:option,
-                                   current_user.send(name),
-                                   :value => "#{scope}_#{current_user.send(identifier)}",
-                                   :selected => true)
-          else
-            options += content_tag(:option,
-                                   user.send(name),
-                                   :value => "#{scope}_#{user.send(identifier)}")
-          end
+      user_proc.call.each do |user|
+        if current_user == user
+          options += content_tag(:option,
+                                 current_user.send(name),
+                                 :value => "#{scope}_#{current_user.send(identifier)}",
+                                 :selected => true)
+        else
+          options += content_tag(:option,
+                                 user.send(name),
+                                 :value => "#{scope}_#{user.send(identifier)}")
         end
       end
-
-      if options.respond_to?(:html_safe)
-        options = options.html_safe
-      end
-      select_tag "switch_user_identifier", options,
-                 :onchange => "location.href = '/switch_user?scope_identifier=' + encodeURIComponent(this.options[this.selectedIndex].value)"
     end
+
+    if options.respond_to?(:html_safe)
+      options = options.html_safe
+    end
+    select_tag "switch_user_identifier", options,
+      :onchange => "location.href = '/switch_user?scope_identifier=' + encodeURIComponent(this.options[this.selectedIndex].value)"
   end
 
   def available?
