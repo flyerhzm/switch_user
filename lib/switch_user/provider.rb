aa0e78e883ae5b1b0e0ffd4398b2e343e38c1dea
Luke Cowell <lcowell@gmail.com>
2012-11-03 17:32:11 -0700

    added a dummy provider
    handles user session management in memory and will allow us to write
    controller tests without loading devise, sorcery etc.

--- lib/switch_user/provider.rb
+++ lib/switch_user/provider.rb
@@ -4,5 +4,6 @@ module SwitchUser
     autoload :Devise, "switch_user/provider/devise"
     autoload :RestfulAuthentication, "switch_user/provider/restful_authentication"
     autoload :Sorcery, "switch_user/provider/sorcery"
+    autoload :Dummy, "switch_user/provider/dummy"
   end
 end
