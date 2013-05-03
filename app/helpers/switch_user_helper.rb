4a1688549a908b06ef3d1672f19d5cee3e96ce24
Luke Cowell <lcowell@gmail.com>
2013-05-02 17:53:45 -0700

    extract helper html to view file

--- app/helpers/switch_user_helper.rb
+++ app/helpers/switch_user_helper.rb
@@ -1,33 +1,29 @@
 module SwitchUserHelper
+  class SelectOption < Struct.new(:label, :scope_id); end
   def switch_user_select
-    # TODO this should be moved in to a view file
     return unless available?
-    options = ''
+    options = []
+    selected_user = nil
 
-    options += content_tag(:option, 'Guest', :value => '', :selected => !current_user) if SwitchUser.helper_with_guest
+    options << SelectOption.new("Guest", "") if SwitchUser.helper_with_guest
     SwitchUser.available_users.each do |scope, user_proc|
-
       current_user = provider.current_user(scope)
       id_name = SwitchUser.available_users_identifiers[scope]
       name = SwitchUser.available_users_names[scope]
 
       user_proc.call.each do |user|
-        user_match = (user == current_user)
-        options += content_tag(:option,
-                               tag_label(user, name),
-                               :value => tag_value(user, id_name, scope),
-                               :selected => user_match)
+        if user == current_user
+          selected_user = tag_value(user, id_name, scope)
+        end
+        options << SelectOption.new(tag_label(user, name), tag_value(user, id_name, scope))
       end
     end
 
-    if options.respond_to?(:html_safe)
-      options = options.html_safe
-    end
-    if SwitchUser.switch_back
-      concat check_box_tag "remember_user", "remember_user", provider.original_user.present?, :onchange => "location.href = 'switch_user/remember_user?remember=' + encodeURIComponent(this.checked)"
-    end
-    select_tag "switch_user_identifier", options,
-      :onchange => "location.href = '/switch_user?scope_identifier=' + encodeURIComponent(this.options[this.selectedIndex].value)"
+    render :partial => "switch_user/widget",
+           :locals => {
+             :options => options,
+             :current_scope => selected_user
+           }
   end
 
   private
