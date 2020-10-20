71e3a3e284dba65422e0f805064b56ac2ad3a87d
 TheSmartnik <misharinn@gmail.com>
2020-10-20 10:53:02 +0300

    Fix zeitwerk autloading issue

--- lib/switch_user/rails.rb
+++ lib/switch_user/rails.rb
@@ -3,8 +3,10 @@
 module SwitchUser
   class Engine < Rails::Engine
     initializer 'switch_user.view' do
-      ActiveSupport.on_load(:action_view) do
-        include SwitchUserHelper
+      config.to_prepare do
+        ActiveSupport.on_load(:action_view) do
+          include SwitchUserHelper
+        end
       end
     end
   end
